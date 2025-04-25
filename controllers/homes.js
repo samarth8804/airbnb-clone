24f5a74a59b584f8082799125a9593ee6e71ff1f
samarth8804 <samarthmittal0808@gmail.com>
2025-04-25 15:13:01 +0530

    separated ejs files

--- controllers/homes.js
+++ controllers/homes.js
@@ -4,7 +4,7 @@ const Home = require("../models/home");
 
 exports.getAddHome = (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "addHome.html"));
-  res.render("addHome", { currentPage: "addHome" });
+  res.render("host/addHome", { currentPage: "addHome" });
 };
 
 exports.homeAdded = (req, res, next) => {
@@ -17,13 +17,13 @@ exports.homeAdded = (req, res, next) => {
 
   // registeredHomes.push({ houseName, houseImage, rating, location, price });
   // res.sendFile(path.join(rootDir, "views", "homeAdded.html"));
-  res.render("homeAdded", { currentPage: "homeAdded" });
+  res.render("host/homeAdded", { currentPage: "homeAdded" });
 };
 
 exports.getHome = (req, res, next) => {
   // console.log(registeredHomes);
   // res.sendFile(path.join(rootDir, "views", "home.html"));
   const registeredHomes = Home.fetchAll((registeredHomes) => {
-    res.render("home", { registeredHomes, currentPage: "home" });
+    res.render("store/homeList", { registeredHomes, currentPage: "home" });
   });
 };
