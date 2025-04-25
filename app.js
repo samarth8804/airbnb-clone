a635aef1258d51f0a2bf4732e818f67776e4b623
samarth8804 <samarthmittal0808@gmail.com>
2025-04-25 15:08:05 +0530

    25-4-25

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const express = require("express");
 const app = express();
 
 const userRouter = require("./routes/userRouter");
-const hostRouter = require("./routes/hostRouter");
+const { hostRouter } = require("./routes/hostRouter");
 const rootDir = require("./utils/pathUtils");
+const { error404 } = require("./controllers/error");
+
+// ejs setup
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(express.urlencoded());
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/user", userRouter);
 
 app.use("/host", hostRouter);
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-});
+app.use(error404);
 
 app.listen(3000);
